f34702ea9aab55b6cadcb1b77f163b42644c85e9
RoyOkite <royokite@gmail.com>
2022-08-08 23:53:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// DOM modifying elements lab!
+const mainDeleter = document.getElementById('main');
+mainDeleter.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.innerHTML = '<p>Rokyt, Apex Legend, is the champion</p>';
